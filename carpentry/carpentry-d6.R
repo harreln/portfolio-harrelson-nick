cec9ccda145ba2d9fe684add0eca2125ea5ff6eb
harreln <harreln@rose-hulman.edu>
2019-05-18 22:21:10 -0400

    prose edits

--- carpentry/carpentry-d6.R
+++ carpentry/carpentry-d6.R
@@ -2,16 +2,17 @@
 
 composite <- read_csv("data-raw/composite-data.csv") %>% as.data.frame()
 
-df <- setNames(composite, c("Material","TensileStrength", "TensileModulus", "Density", "SpecificStrength", "SpecificModulus","UltimateStrain","Manufacturer","Type"))
+df <- setNames(composite, c("Material","Tensile Strength\n[MPa]", "Tensile Modulus\n[GPa]", "Density\n[g/cc]", "Specific Strength\n[MPa/(g/cc)]", "Specific Modulus\n[GPa/(g/cc)]","Ultimate Strain","Manufacturer","Type"))
 
 df <- df[ , -which(names(df) %in% c("Manufacturer"))] 
 
-df$Type[df$Type=="Bulk polymers"] <- "Bulk polymer matrix materials"
-df$Type[df$Type=="Other fibers"] <- "Glass fibers"
-df$Type[df$Type=="PAN-Based Carbon Fibers"] <- "Carbon Fibers"
-df$Type[df$Type=="Pitch-Based Carbon Fibers"] <- "Carbon Fibers"
+df$Type[df$Type=="Bulk polymer matrix materials"] <- "Bulk polymers"
+df$Type[df$Type=="Glass fibers"] <- "Fibers"
+df$Type[df$Type=="Other fibers"] <- "Fibers"
+df$Type[df$Type=="PAN-Based Carbon Fibers"] <- "Fibers"
+df$Type[df$Type=="Pitch-Based Carbon Fibers"] <- "Fibers"
+df$Type[df$Type=="Polymetric Fibers"] <- "Fibers"
 
 df$Type <- as.factor(df$Type)
-
 
 saveRDS(df, "data/d6.rds")
--- design/design-d6.R
+++ design/design-d6.R
@@ -1,22 +1,28 @@
 library("scagnostics")
+library(ggplot2)
+library(GGally)
+
 
 df <- readRDS("data/d6.rds")
 
-mytitle <- "Comparing Material Properties of Composites (values normalized)"
+mytitle <- "Comparing Material Properties of Composite Material Components"
 
-ggparcoord(data = df, 
-           columns = 2:7, 
-           groupColumn  = "Type",
-           scale = "center", 
-           order = "Skewed", 
-           mapping = ggplot2::aes(size = 1, alpha = 0.4),
-           title = mytitle) +
-  ggplot2::scale_size_identity() +
-  labs(x = "", y = "")
-  
+ggscatmat(na.omit(df), columns = 2:7, color = "Type") +
+  geom_point(size = 1, alpha = 0.1, na.rm = TRUE, shape = 21) + 
+  theme(legend.position = "right",
+        panel.spacing = unit(1, "mm"),  
+        axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_color_manual(values = brewer.pal(name = "Dark2", n = 3)) +
+  labs(title = mytitle,
+       caption = "Source: Principles of Composite Material Mechanics, Ronald F. Gibson",
+       color = "Material Type") 
+
+
+
+
 ggsave(filename = "d6.png",
        path = "figures",
-       width = 7,
-       height = 4,
+       width = 8,
+       height = 8,
        units = "in",
        dpi = 300)
